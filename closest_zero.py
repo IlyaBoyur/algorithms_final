5163ba31c64775810e7a883e9b4f35ee56df5761
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 19:24:06 +0300

    Remove unnecessary variable

--- closest_zero.py
+++ closest_zero.py
@@ -6,7 +6,7 @@ def closest_zero(street):
     # Before first zero
     for i in range(zeros[0]):
         result[i] = zeros[0] - i
-    # # Between first and last zeros (if any)
+    # Between first and last zeros (if any)
     first_zero = zeros[0]
     for zero in zeros[1:]:
         for i in range(first_zero, zero):
@@ -20,5 +20,4 @@ def closest_zero(street):
 
 if __name__ == '__main__':
     input()
-    street = input()
-    print(closest_zero(street.rstrip().split(' ')))
+    print(closest_zero(input().rstrip().split(' ')))
