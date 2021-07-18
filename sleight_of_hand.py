48a8bd27a91df1a32d7428b82f56dc30b89f9eec
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 20:45:39 +0300

    Remove digit to str cast in generator

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -1,9 +1,9 @@
 # Solution ID: 52177009
 def sleight_of_hand(keys_count,  matrix_cells, players_count=2,
-                    numbers_list=range(1, 10)):
+                    game_digits=['1','2','3','4','5','6','7','8','9']):
     fingers_count = keys_count * players_count
-    return sum(0 < matrix_cells.count(f'{digit}') <= fingers_count
-                for digit in numbers_list)
+    return sum(0 < matrix_cells.count(digit) <= fingers_count
+                for digit in game_digits)
 
 
 if __name__ == '__main__':
