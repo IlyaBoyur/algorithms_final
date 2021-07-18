a1b23e82a0faa57b3bf4c357a1239c875b4d28ed
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 20:29:08 +0300

    Move constants to parameter list

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -1,8 +1,9 @@
 # Solution ID: 52177009
-def sleight_of_hand(keys_count, matrix_cells):
-    k_x2 = keys_count * 2
-    return sum([0 < matrix_cells.count(f'{digit}') <= k_x2
-                for digit in range(10)])
+def sleight_of_hand(keys_count,  matrix_cells, players_count=2,
+                    numbers_list=range(1, 10)):
+    fingers_count = keys_count * players_count
+    return sum([0 < matrix_cells.count(f'{digit}') <= fingers_count
+                for digit in numbers_list])
 
 
 if __name__ == '__main__':
