ff7d3366ed8d918a5f54c59624229e2299816e00
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 19:48:47 +0300

    Remove shortened variables

--- closest_zero.py
+++ closest_zero.py
@@ -1,28 +1,28 @@
 # Solution ID: 52177445
 def closest_zero(street):
     street_length = len(street)
-    zeros = [i for (i, e) in enumerate(street) if e == '0']
+    zeros = [index for (index, number) in enumerate(street) if number == '0']
     result = [0] * street_length
     # Before first zero
     first_zero = zeros[0]
-    for i in range(first_zero):
-        result[i] = first_zero - i
+    for number in range(first_zero):
+        result[number] = first_zero - number
     # Between first and last zeros (if any)
     left_zero = first_zero
     for right_zero in zeros[1:]:
-        for i in range(left_zero, right_zero):
-            result[i] = min(i - left_zero, right_zero - i)
+        for number in range(left_zero, right_zero):
+            result[number] = min(number - left_zero, right_zero - number)
         left_zero = right_zero
     # After last zero
     last_zero = zeros[-1]
-    for i in range(last_zero, street_length):
-        result[i] = i - last_zero
+    for number in range(last_zero, street_length):
+        result[number] = number - last_zero
     return result
 
 
 if __name__ == '__main__':
     input()
     print(' '.join([
-        str(i)
-        for i in closest_zero(input().rstrip().split(' '))
+        str(number)
+        for number in closest_zero(input().rstrip().split(' '))
     ]))
