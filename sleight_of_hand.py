1f667ab4dfc0db0a81ac70f4a3604ccb18e79dfc
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 20:19:21 +0300

    Remove unneeded variables and constant from main

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -1,7 +1,4 @@
 # Solution ID: 52177009
-MATRIX_ROW_COUNT = 4
-
-
 def sleight_of_hand(keys_count, matrix_cells):
     k_x2 = keys_count * 2
     return sum([0 < matrix_cells.count(f'{digit}') <= k_x2
@@ -9,8 +6,5 @@ def sleight_of_hand(keys_count, matrix_cells):
 
 
 if __name__ == '__main__':
-    keys_count = int(input())
-    matrix_cells = [''] * MATRIX_ROW_COUNT
-    for i in range(MATRIX_ROW_COUNT):
-        matrix_cells[i] = input().rstrip()
-    print(sleight_of_hand(keys_count, ''.join(matrix_cells)))
+    print(sleight_of_hand(keys_count=int(input()),
+                          matrix_cells=input()+input()+input()+input()))
