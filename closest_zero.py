78e1e8b01549c1c83c8c182ae75206fa0c017336
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 19:41:04 +0300

    Use new variables to lessen zeros list lookup count

--- closest_zero.py
+++ closest_zero.py
@@ -4,17 +4,19 @@ def closest_zero(street):
     zeros = [i for (i, e) in enumerate(street) if e == '0']
     result = [0] * street_length
     # Before first zero
-    for i in range(zeros[0]):
-        result[i] = zeros[0] - i
+    first_zero = zeros[0]
+    for i in range(first_zero):
+        result[i] = first_zero - i
     # Between first and last zeros (if any)
-    left_zero = zeros[0]
+    left_zero = first_zero
     for right_zero in zeros[1:]:
         for i in range(left_zero, right_zero):
             result[i] = min(i - left_zero, right_zero - i)
         left_zero = right_zero
     # After last zero
-    for i in range(zeros[-1], street_length):
-        result[i] = i - zeros[-1]
+    last_zero = zeros[-1]
+    for i in range(last_zero, street_length):
+        result[i] = i - last_zero
     return result
 
 
