29691eb2a050242de552ff03d341d570b3c4621c
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 20:32:33 +0300

    Optimise time: remove list creation for generator

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -2,8 +2,8 @@
 def sleight_of_hand(keys_count,  matrix_cells, players_count=2,
                     numbers_list=range(1, 10)):
     fingers_count = keys_count * players_count
-    return sum([0 < matrix_cells.count(f'{digit}') <= fingers_count
-                for digit in numbers_list])
+    return sum(0 < matrix_cells.count(f'{digit}') <= fingers_count
+                for digit in numbers_list)
 
 
 if __name__ == '__main__':
