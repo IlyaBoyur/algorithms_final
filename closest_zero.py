f22a9c6a93d3380eb8e151f99cc56abee51cc282
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-18 11:55:53 +0300

    Clearify closest zero
    * remove append
    * distinguish before 1st, between zeros and after last parts
    * remove unneeded street length parameter
    * remove d, d_next

--- closest_zero.py
+++ closest_zero.py
@@ -1,28 +1,24 @@
-# Solution ID: 52168494
-def closest_zero(street_length, street):
-    zeros = [
-        i for (i, e) in enumerate(street.split(' ')[:street_length])
-        if e == '0'
-    ]
-    z_i = 0
-    z_len = len(zeros)
-    result = []
-    for i in range(0, street_length):
-        if (z_i == z_len-1):
-            digit = abs(i - zeros[z_i])
-        else:
-            d = abs(i - zeros[z_i])
-            d_next = abs(i - zeros[z_i + 1])
-
-            digit = min(d, d_next)
-
-            if i == zeros[z_i + 1]:
-                z_i += 1
-        result.append(digit)
+# Solution ID: 52177413
+def closest_zero(street):
+    street_length = len(street)
+    zeros = [i for (i, e) in enumerate(street) if e == '0']
+    result = [0] * street_length
+    # Before first zero
+    for i in range(zeros[0]):
+        result[i] = zeros[0] - i
+    # # Between first and last zeros (if any)
+    first_zero = zeros[0]
+    for zero in zeros[1:]:
+        for i in range(first_zero, zero):
+            result[i] = min(i - first_zero, zero - i)
+        first_zero = zero
+    # After last zero
+    for i in range(zeros[-1], street_length):
+        result[i] = i - zeros[-1]
     return(' '.join([str(i) for i in result]))
 
 
 if __name__ == '__main__':
-    street_length = int(input())
+    input()
     street = input()
-    print(closest_zero(street_length, street))
+    print(closest_zero(street.rstrip().split(' ')))
