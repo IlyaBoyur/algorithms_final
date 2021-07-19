2991d41f48e73f7eed9e2a8a5950b1b48633a8e2
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-19 07:14:20 +0300

    Remove parameters which cannot be used correctly

--- closest_zero.py
+++ closest_zero.py
@@ -1,25 +1,24 @@
 # Solution ID: 52179765
-def closest_zero(street, empty_pose_value='0', first_zero_index=0,
-                 last_zero_index=-1):
+def closest_zero(street, empty_pose_value='0'):
     street_length = len(street)
     zeros = [
         index
         for (index, number) in enumerate(street)
         if number == empty_pose_value
     ]
-    result = [int(empty_pose_value)] * street_length
+    result = [0] * street_length
     # Before first zero
-    first_zero = zeros[first_zero_index]
+    first_zero = zeros[0]
     for number in range(first_zero):
         result[number] = first_zero - number
     # Between first and last zeros (if any)
     left_zero = first_zero
-    for right_zero in zeros[first_zero_index+1:]:
+    for right_zero in zeros[1:]:
         for number in range(left_zero, right_zero):
             result[number] = min(number - left_zero, right_zero - number)
         left_zero = right_zero
     # After last zero
-    last_zero = zeros[last_zero_index]
+    last_zero = zeros[-1]
     for number in range(last_zero, street_length):
         result[number] = number - last_zero
     return result
