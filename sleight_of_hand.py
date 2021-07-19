d75aef699c1f862bc9e9cc544c2d6de847149365
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-19 07:01:46 +0300

    Fix code style errors and game_digits

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -1,11 +1,11 @@
 # Solution ID: 52179761
 def sleight_of_hand(keys_count,  matrix_cells, players_count=2,
-                    game_digits=['1','2','3','4','5','6','7','8','9']):
+                    game_digits='123456789'):
     fingers_count = keys_count * players_count
     return sum(0 < matrix_cells.count(digit) <= fingers_count
-                for digit in game_digits)
+               for digit in game_digits)
 
 
 if __name__ == '__main__':
     print(sleight_of_hand(keys_count=int(input()),
-                          matrix_cells=input()+input()+input()+input()))
+                          matrix_cells=input() + input() + input() + input()))
