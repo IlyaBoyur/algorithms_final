1d40f9b0f922f6afdfdd69dde0d68b50f797cc55
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-21 20:07:01 +0300

    Move reverse polish calc operations to dictionary

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -1,11 +1,17 @@
 # Solution ID: 52187536
 ERROR_STACK_EMPTY = 'stack empty error'
-ERROR_INVALID_INPUT = 'invalid input error'
+ERROR_INVALID_OPERATOR = 'invalid operator error'
 ERROR_ZERO_DIVISION = 'zero division error'
 MINUS = '-'
 PLUS = '+'
 MULTIPLY = '*'
 DIVIDE = '/'
+OPERATOR_TO_FUNCTION = {
+    MINUS: lambda operand_left, operand_right: operand_left - operand_right,
+    PLUS: lambda operand_left, operand_right: operand_left + operand_right,
+    MULTIPLY: lambda operand_left, operand_right: operand_left * operand_right,
+    DIVIDE: lambda operand_left, operand_right: operand_left // operand_right,
+}
 
 
 class Stack:
@@ -25,27 +31,21 @@ def reverse_polish_calculate(operands):
     stack = Stack()
     for argument in operands:
         try:
-            stack.push(int(argument))
+            new_value = int(argument)
         except ValueError:
-            if argument == PLUS:
-                stack.push(stack.pop() + stack.pop())
-            elif argument == MULTIPLY:
-                stack.push(stack.pop() * stack.pop())
-            elif argument == MINUS:
-                subtrahend = stack.pop()
-                stack.push(stack.pop() - subtrahend)
-            elif argument == DIVIDE:
-                divider = stack.pop()
-                stack.push(stack.pop() // divider)
-            else:
-                return ERROR_INVALID_INPUT
-        except ZeroDivisionError:
-            return ERROR_ZERO_DIVISION
+            if argument not in OPERATOR_TO_FUNCTION:
+                raise ValueError(ERROR_INVALID_OPERATOR)
+            operand_right, operand_left = stack.pop(), stack.pop()
+            new_value = OPERATOR_TO_FUNCTION[argument](operand_left,
+                                                       operand_right)
+        stack.push(new_value)
     return stack.pop()
 
 
 if __name__ == '__main__':
     try:
         print(reverse_polish_calculate(input().split(' ')))
+    except ValueError:
+        pass
     except RuntimeError:
         pass
