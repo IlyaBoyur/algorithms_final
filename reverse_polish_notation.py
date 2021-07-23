d61f3b123a6a931cd4531b5eb108543c5c8d7453
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-23 08:31:10 +0300

    Move operators to argument list

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -23,15 +23,16 @@ class Stack:
             raise RuntimeError(ERROR_STACK_EMPTY)
 
 
-def reverse_polish_calculate(operands, stack=Stack(), digitizer=int):
+def reverse_polish_calculate(operands, stack=Stack(), digitizer=int,
+                             operators=OPERATORS):
     for argument in operands:
         try:
             new_value = digitizer(argument)
         except ValueError:
-            if argument not in OPERATORS:
+            if argument not in operators:
                 raise ValueError(ERROR_INVALID_OPERATOR)
             operand_right, operand_left = stack.pop(), stack.pop()
-            new_value = OPERATORS[argument](operand_left,
+            new_value = operators[argument](operand_left,
                                             operand_right)
         stack.push(new_value)
     return stack.pop()
