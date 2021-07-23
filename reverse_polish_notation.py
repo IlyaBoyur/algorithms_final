1ef799eb599484cb18831e9d6403b06026dc6d58
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-23 08:18:44 +0300

    Simplify names and constants

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -1,15 +1,11 @@
 # Solution ID: 52202637
 ERROR_STACK_EMPTY = 'stack empty error'
 ERROR_INVALID_OPERATOR = 'invalid operator error'
-MINUS = '-'
-PLUS = '+'
-MULTIPLY = '*'
-DIVIDE = '/'
-OPERATOR_TO_FUNCTION = {
-    MINUS: lambda operand_left, operand_right: operand_left - operand_right,
-    PLUS: lambda operand_left, operand_right: operand_left + operand_right,
-    MULTIPLY: lambda operand_left, operand_right: operand_left * operand_right,
-    DIVIDE: lambda operand_left, operand_right: operand_left // operand_right,
+OPERATORS = {
+    '-': lambda left, right: left - right,
+    '+': lambda left, right: left + right,
+    '*': lambda left, right: left * right,
+    '/': lambda left, right: left // right,
 }
 
 
@@ -32,11 +28,11 @@ def reverse_polish_calculate(operands):
         try:
             new_value = int(argument)
         except ValueError:
-            if argument not in OPERATOR_TO_FUNCTION:
+            if argument not in OPERATORS:
                 raise ValueError(ERROR_INVALID_OPERATOR)
             operand_right, operand_left = stack.pop(), stack.pop()
-            new_value = OPERATOR_TO_FUNCTION[argument](operand_left,
-                                                       operand_right)
+            new_value = OPERATORS[argument](operand_left,
+                                            operand_right)
         stack.push(new_value)
     return stack.pop()
 
