223f5937a87b35e9d45eac3ca9c37f8f766e30eb
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-07-25 06:43:59 +0300

    Remove partition from global avalilability

--- effective_quick_sort.py
+++ effective_quick_sort.py
@@ -1,22 +1,19 @@
 # Solution ID: 52222119
-def partition(array, left, right, reverse=False):
-    pivot = array[right]
-    partition_index = left-1
-    for index in range(left, right):
-        shall_swap = array[index] > pivot if reverse else array[index] < pivot
-        if shall_swap:
-            partition_index += 1
-            array[partition_index], array[index] = (array[index],
-                                                    array[partition_index])
-    array[partition_index + 1], array[right] = (array[right],
-                                                array[partition_index + 1])
-    return partition_index + 1
-
-
 def quick_sort(array, left, right, reverse=False):
+    def partition(left, right, reverse=False):
+        pivot = array[right]
+        partition_index = left-1
+        for index in range(left, right):
+            if array[index] < pivot:
+                partition_index += 1
+                array[partition_index], array[index] = (array[index],
+                                                        array[partition_index])
+        array[partition_index + 1], array[right] = (array[right],
+                                                    array[partition_index + 1])
+        return partition_index + 1
     if right <= left:
         return
-    partition_index = partition(array, left, right, reverse)
+    partition_index = partition(left, right, reverse)
     quick_sort(array, left, partition_index - 1, reverse)
     quick_sort(array, partition_index + 1, right, reverse)
 
